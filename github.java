dab1d3adeac4f00ca73f3373ba310803fbde7a7c
ShivShakti7445 <141945568+ShivShakti7445@users.noreply.github.com>
2023-09-17 12:12:10 +0530

    Create github.java

---
+++ github.java
@@ -0,0 +1,18 @@
+public class github {
+    
+p
+    public static void main (String[]args){
+     char i,j,k;
+      for(i='A';i<='E';i++){
+       for(k='E';k>i;k--){
+         System.out.print( " ");
+   
+       }
+       for(j='A';j<=i;j++){
+      
+        System.out.print(j+"  ");}
+        System.out.print("\n ");}
+         }
+            }
+            
+
